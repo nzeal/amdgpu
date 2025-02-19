1af8a09f1054e39c0d34ff2b193ede7f6f97e2aa
nshukla1 <n.shukla@cineca.it>
2025-02-19 14:57:27 +0100

    printing correctness of the result

--- multipleKernel/cudafun/matrix_mul_kernel1.cu
+++ multipleKernel/cudafun/matrix_mul_kernel1.cu
@@ -1,15 +1,15 @@
 #include "../includes/matrix_mul_kernel.h"
 
-__global__ void matrixMulKernel1(const double *A, const double *B, double *C, int m, int n, int k) {
+__global__ void matrixMulKernel1(const double *A, const double *B, double *C, int M, int N, int K, double alpha, double beta) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (row < m && col < n) {
-        double sum = 0.0;
-        for (int i = 0; i < k; i++) {
-            sum += A[row * k + i] * B[i * n + col];
+    if (row < M && col < N) {
+        double acc_c = 0.0;
+        for (int k = 0; k < K; k++) {
+            acc_c += A[row * K + k] * B[k * N + col];
         }
-        C[row * n + col] = sum;
+        C[row * N + col] = alpha * acc_c + beta * C[row * N + col];
     }
 }
 
--- multipleKernel/cudafun/matrix_mul_kernel2.cu
+++ multipleKernel/cudafun/matrix_mul_kernel2.cu
@@ -1,15 +1,15 @@
 #include "../includes/matrix_mul_kernel.h"
 
-__global__ void matrixMulKernel2(const double *A, const double *B, double *C, int M, int N, int K) {
-    int row = blockIdx.y * blockDim.y + threadIdx.y;  // maps to i
-    int col = blockIdx.x * blockDim.x + threadIdx.x;  // maps to j
+__global__ void matrixMulKernel2(const double *A, const double *B, double *C, int M, int N, int K, double alpha, double beta) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (row < M && col < N) {
-        double sum = 0.0;
+        double acc_c = 0.0;
         for (int k = 0; k < K; k++) {
-            sum += A[row * K + k] * B[k * N + col];
+            acc_c += A[row * K + k] * B[k * N + col];
         }
-        C[row * N + col] = sum;
+        C[row * N + col] = alpha * acc_c + beta * C[row * N + col];
     }
 }
 
